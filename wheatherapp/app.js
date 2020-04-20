ef467a674b441053da7b82909445fd48d24fa469
karishma <kkunwar211@gmail.com>
2020-04-20 15:52:15 +0545

    work with openwhether API

--- wheatherapp/app.js
+++ wheatherapp/app.js
@@ -1,11 +1,34 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+    a:{
+        demand:true,
+        address:'address',
+        describe: 'This gives the address',
+        string: true
+    }
+
+})
+.help()
+.alias('help','h')
+.argv;
+console.log(argv);
+
+
+//lets get the encoded address
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url :'https://api.openweathermap.org/data/2.5/weather?q=Nepal&appid=16d2fddbf87288b88787b57f8a7bd366',
+    url :`https://api.openweathermap.org/data/2.5/weather?q=${encodedAddress}&appid=16d2fddbf87288b88787b57f8a7bd366`,
     json:true
 },(err,response,body) =>{
 
-    console.log(body);
+   
+   
+    console.log(`Printing the longitude and latitude : ${body.coord.lon}`);
+
+
 
 
 });
