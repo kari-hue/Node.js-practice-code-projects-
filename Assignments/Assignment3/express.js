139b744f7be2b3c6fe6f57b444c41312569d182e
karishma <kkunwar211@gmail.com>
2020-04-24 14:13:02 +0545

    connecting routes with html files

--- Assignments/Assignment3/express.js
+++ Assignments/Assignment3/express.js
@@ -1,13 +1,48 @@
 
 
-const express = require("express");
+const express = require('express');
+const app = express();
 
 const path = require("path");
-const app = express();
+//const router = express.Router();
 const port = 3000;
 
+// app.use((res,req) => {
+
+//     res.sendFile(path.join(__dirname,'htmlcontent','pagenotfound.html'));
+//     res.redirect('/');
+    
+// });
+
+app.get('/users',(req,res) =>{
+
+    // app.setTimeout(() => {
+
+        res.sendFile(path.join(__dirname,'/','htmlcontent','pagefound.html'));
 
 
+        
+    // }, 5000);
+   
+
+
+    // res.redirect('/');
+
+});
+
+app.get('/',(req,res) =>{
+
+    res.send('<h1>Hello you are in the main page</h1>');
+
+})
+
+app.use((res,req,next)=>{
+
+    res.status(404).sendFile(path.join(__dirname,'htmlcontent','pagenotfound.html'));
+
+    
+
+});
 
 
 
