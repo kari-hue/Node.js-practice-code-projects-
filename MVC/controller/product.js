69d3131d10cc20aceb348ebaa2a20e78d5a77b58
karishma <kkunwar211@gmail.com>
2020-04-29 15:10:08 +0545

    controller module

---
+++ MVC/controller/product.js
@@ -0,0 +1,32 @@
+
+// const products = [];
+const Product = require('../models/productq');
+
+exports.getaddProduct = (req, res, next) => {
+    res.render('add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+}
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Products(req.body.title);
+    product.save();
+    res.redirect('/');
+  }
+
+  exports.getProducts = (req, res, next) => {
+    
+    const products = Product.fetchAll();
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  }
